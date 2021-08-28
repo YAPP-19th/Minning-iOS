757c1fb7a4f4d45f116849c81455c82ab66b4dc0
della-padula <della.kimko@gmail.com>
2021-08-28 20:45:27 +0900

    [YAPPIOS-8] Pod 설정, Organization 설정, Scene 관련 파일 제거

---
+++ Pods/Target Support Files/Pods-OurApp-OurAppUITests/Pods-OurApp-OurAppUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_OurApp_OurAppUITests : NSObject
+@end
+@implementation PodsDummy_Pods_OurApp_OurAppUITests
+@end
---
+++ Pods/Target Support Files/Pods-OurAppTests/Pods-OurAppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_OurAppTests : NSObject
+@end
+@implementation PodsDummy_Pods_OurAppTests
+@end
