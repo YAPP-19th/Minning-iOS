6db29b61761f25ce6fa9804c534d3d4dc27d9c4a
koserim <btobang1@naver.com>
2021-10-12 00:15:22 +0900

    [YAPPIOS-50] collectionView section enum으로 관리

--- Minning/ViewControllers/Main/Home/Routine/RoutineView.swift
+++ Minning/ViewControllers/Main/Home/Routine/RoutineView.swift
@@ -13,6 +13,14 @@ protocol RoutineViewDelegate: AnyObject {
 }
 
 class RoutineView: UIView {
+    
+    enum CollectionViewSection: Int, CaseIterable {
+        case header
+        case phraseGuide
+        case routine
+        case review
+    }
+    
     weak var delegate: RoutineViewDelegate?
 
     lazy var mainCollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
@@ -48,18 +56,22 @@ class RoutineView: UIView {
 
 extension RoutineView: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        switch section {
-        case 0:
+        switch CollectionViewSection(rawValue: section) {
+        case .header:
+            return 0
+        case .phraseGuide:
             return 1
-        case 1:
+        case .routine:
             return 3
+        case .review:
+            return 0
         default:
             return 0
         }
     }
         
     func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 2
+        return CollectionViewSection.allCases.count
     }
 }
 
@@ -72,28 +84,36 @@ extension RoutineView: UICollectionViewDelegate {
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        switch indexPath.section {
-        case 0:
+        switch CollectionViewSection(rawValue: indexPath.section) {
+        case .header:
+            return .init()
+        case .phraseGuide:
             guard let cell = mainCollectionView.dequeueReusableCell(withReuseIdentifier: PhraseGuideCell.identifier, for: indexPath) as? PhraseGuideCell else {
                 return UICollectionViewCell()
             }
             return cell
-        case 1:
+        case .routine:
             guard let cell = mainCollectionView.dequeueReusableCell(withReuseIdentifier: RoutineCollectionViewCell.identifier, for: indexPath) as? RoutineCollectionViewCell else {
                 return UICollectionViewCell()
             }
             cell.configure()
             return cell
+        case .review:
+            return .init()
         default:
-            return UICollectionViewCell()
+            return .init()
         }
     }
         
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        switch indexPath.section {
-        case 0:
+        switch CollectionViewSection(rawValue: indexPath.section) {
+        case .header:
+            break
+        case .phraseGuide:
             delegate?.didSelectPhraseGuide()
-        case 1:
+        case .routine:
+            break
+        case .review:
             break
         default:
             break
@@ -103,32 +123,56 @@ extension RoutineView: UICollectionViewDelegate {
 
 extension RoutineView: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
-        switch section {
-        case 0:
-            return .init(width: frame.width - 32, height: 50)
-        case 1:
-            return .zero
+        switch CollectionViewSection(rawValue: section) {
+        case .header:
+            return .init(width: frame.width - 32, height: 56)
         default:
             return .zero
         }
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        switch indexPath.section {
-        case 0:
+        switch CollectionViewSection(rawValue: indexPath.section) {
+        case .header:
+            return .zero
+        case .phraseGuide:
             return .init(width: frame.width - 32, height: 50)
-        case 1:
+        case .routine:
             return .init(width: frame.width - 32, height: 70)
+        case .review:
+            return .zero
         default:
             return .zero
         }
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return .init(top: 14, left: 16, bottom: .zero, right: 16)
+        switch CollectionViewSection(rawValue: section) {
+        case .header:
+            return .zero
+        case .phraseGuide:
+            return .init(top: 14, left: 16, bottom: .zero, right: 16)
+        case .routine:
+            return .init(top: 14, left: 16, bottom: .zero, right: 16)
+        case .review:
+            return .zero
+        default:
+            return .zero
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 8
+        switch CollectionViewSection(rawValue: section) {
+        case .header:
+            return .zero
+        case .phraseGuide:
+            return .zero
+        case .routine:
+            return 8
+        case .review:
+            return .zero
+        default:
+            return .zero
+        }
     }
 }
