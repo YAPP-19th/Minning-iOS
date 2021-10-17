56deb64f77d25d9cfe32f66cfbab85bf9c47d5cb
koserim <btobang1@naver.com>
2021-10-17 19:41:46 +0900

    [YAPPIOS-51] 순서 편집용 셀 생성 및 순서 편집 기능 추가

--- Minning/ViewControllers/Main/Home/EditOrder/EditOrderViewController.swift
+++ Minning/ViewControllers/Main/Home/EditOrder/EditOrderViewController.swift
@@ -13,7 +13,21 @@ import SharedAssets
 import SnapKit
 
 final class EditOrderViewController: BaseViewController {
+    lazy var mainTableView: UITableView = {
+        $0.separatorStyle = .none
+        $0.delegate = self
+        $0.dataSource = self
+        $0.dragInteractionEnabled = true
+        $0.dragDelegate = self
+        $0.dropDelegate = self
+        $0.contentInset = .init(top: 38, left: 0, bottom: 0, right: 0)
+        $0.register(EditOrderCell.self, forCellReuseIdentifier: EditOrderCell.identifier)
+        return $0
+    }(UITableView())
+    
     private let viewModel: EditOrderViewModel
+    
+    private var tempDataList = ["아침에 신문~", "모닝 커피", "달리기"]
     
     init(viewModel: EditOrderViewModel) {
         self.viewModel = viewModel
@@ -32,6 +46,7 @@ final class EditOrderViewController: BaseViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupViewLayout()
     }
     
     private func setupNavigationBar() {
@@ -59,5 +74,60 @@ final class EditOrderViewController: BaseViewController {
     
     override func setupViewLayout() {
         view.backgroundColor = .minningLightGray100
+        mainTableView.backgroundColor = .clear
+
+        [mainTableView].forEach {
+            view.addSubview($0)
+        }
+                
+        mainTableView.snp.makeConstraints { make in
+            make.leading.trailing.bottom.equalToSuperview()
+            make.top.equalToSuperview().offset(0)
+        }
+    }
+}
+
+extension EditOrderViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: EditOrderCell.identifier, for: indexPath) as? EditOrderCell else {
+            return .init()
+        }
+        cell.configure(title: tempDataList[indexPath.row])
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let moveCell = self.tempDataList[sourceIndexPath.row]
+        self.tempDataList.remove(at: sourceIndexPath.row)
+        self.tempDataList.insert(moveCell, at: destinationIndexPath.row)
+    }
+}
+
+extension EditOrderViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 78
+    }
+}
+
+extension EditOrderViewController: UITableViewDragDelegate {
+    func tableView(_ tableView: UITableView, itemsForBeginning session: UIDragSession, at indexPath: IndexPath) -> [UIDragItem] {
+        return [UIDragItem(itemProvider: NSItemProvider())]
+    }
+}
+
+extension EditOrderViewController: UITableViewDropDelegate {
+    func tableView(_ tableView: UITableView, dropSessionDidUpdate session: UIDropSession, withDestinationIndexPath destinationIndexPath: IndexPath?) -> UITableViewDropProposal {
+        if session.localDragSession != nil {
+            return UITableViewDropProposal(operation: .move, intent: .insertAtDestinationIndexPath)
+        }
+        return UITableViewDropProposal(operation: .cancel, intent: .unspecified)
+    }
+    
+    func tableView(_ tableView: UITableView, performDropWith coordinator: UITableViewDropCoordinator) {
+        
     }
 }
---
+++ Minning/ViewControllers/Main/Home/EditOrder/Views/EditOrderCell.swift
@@ -0,0 +1,80 @@
+//
+//  EditOrderCell.swift
+//  Minning
+//
+//  Created by 고세림 on 2021/10/17.
+//  Copyright © 2021 Minning. All rights reserved.
+//
+
+import UIKit
+
+final class EditOrderCell: UITableViewCell {
+    static let identifier = "EditOrderCell"
+    
+    private let categoryBarView = UIView()
+    private let titleLabel = UILabel()
+    private let editOrderImageView: UIImageView = {
+        $0.image = UIImage(sharedNamed: "list_selection.png")
+        return $0
+    }(UIImageView())
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupViewLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure(title: String) {
+        setTempData()
+        titleLabel.text = title
+    }
+    
+    private func setupViewLayout() {
+        selectionStyle = .none
+        backgroundColor = .clear
+        
+        contentView.backgroundColor = .primaryWhite
+        contentView.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMaxXMaxYCorner]
+        contentView.layer.cornerRadius = 10
+        contentView.layer.masksToBounds = true
+        
+        layer.shadowRadius = 4
+        layer.shadowOpacity = 1
+        layer.shadowOffset = CGSize(width: 0, height: 0)
+        layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.05).cgColor
+        
+        [categoryBarView, titleLabel, editOrderImageView].forEach {
+            contentView.addSubview($0)
+        }
+        
+        titleLabel.font = .font16PBold
+        
+        contentView.snp.makeConstraints { make in
+            make.leading.equalTo(16)
+            make.trailing.equalTo(-16)
+            make.top.equalTo(4)
+            make.bottom.equalTo(-4)
+        }
+        categoryBarView.snp.makeConstraints { make in
+            make.top.bottom.leading.equalToSuperview()
+            make.width.equalTo(3)
+        }
+        titleLabel.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.leading.equalTo(categoryBarView.snp.trailing).offset(12)
+        }
+        editOrderImageView.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.trailing.equalToSuperview().offset(-12)
+            make.width.equalTo(12)
+            make.height.equalTo(7)
+        }
+    }
+    
+    private func setTempData() {
+        categoryBarView.backgroundColor = .minningBlue100
+    }
+}
