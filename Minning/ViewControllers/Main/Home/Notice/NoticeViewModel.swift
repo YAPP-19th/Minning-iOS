c7808bd586736a6be541ef2ccb1e4a6c92095bc7
della-padula <della.kimko@gmail.com>
2021-10-30 23:28:08 +0900

    [YAPPIOS-118] 마이페이지 > 공지사항 뷰 구현 및 샘플 데이터 적용

--- Minning/ViewControllers/Main/Home/Notice/NoticeViewModel.swift
+++ Minning/ViewControllers/Main/Home/Notice/NoticeViewModel.swift
@@ -10,9 +10,27 @@ import CommonSystem
 import Foundation
 
 final class NoticeViewModel {
+    // SAMPLE
+    var sampleNotiList: DataBinding<[NotificationModel]> = DataBinding([])
+    
     private let coordinator: HomeCoordinator
     
     public init(coordinator: HomeCoordinator) {
         self.coordinator = coordinator
+    }
+    
+    func createSampleDate() {
+        for _ in 0..<5 {
+            var list = sampleNotiList.value
+            let notification = NotificationModel(title: "공지 제목입니다",
+                                                 description: "공지 내용이 나오는 부분입니다",
+                                                 date: Date())
+            list.append(notification)
+            sampleNotiList.accept(list)
+        }
+    }
+    
+    public func goToBack() {
+        coordinator.goToBack()
     }
 }
