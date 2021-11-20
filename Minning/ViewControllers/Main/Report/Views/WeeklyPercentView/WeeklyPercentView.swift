8430540fa05e0cedd900c33a72cb4968b76b4b84
koserim <btobang1@naver.com>
2021-11-21 02:19:29 +0900

    [YAPPIOS-158] 주간 달성률 뷰 말풍선 추가 작업

--- Minning/ViewControllers/Main/Report/Views/WeeklyPercentView/WeeklyPercentView.swift
+++ Minning/ViewControllers/Main/Report/Views/WeeklyPercentView/WeeklyPercentView.swift
@@ -11,6 +11,7 @@ import DesignSystem
 import Foundation
 import SharedAssets
 import SnapKit
+import UIKit
 
 final class WeeklyPercentView: UIView {
     private let titleLabel: UILabel = {
@@ -22,16 +23,10 @@ final class WeeklyPercentView: UIView {
     
     private let percentGuideButton: UIButton = {
         $0.setImage(UIImage(sharedNamed: "questionButton.png"), for: .normal)
+        $0.addTarget(self, action: #selector(onClickPercentGuideButton(_:)), for: .touchUpInside)
         return $0
     }(UIButton())
-    
-    private let titleStackView: UIStackView = {
-        $0.axis = .horizontal
-        $0.spacing = 6
-        $0.alignment = .bottom
-        return $0
-    }(UIStackView())
-    
+
     private let percentValueLabel: UILabel = {
         $0.text = "90"
         $0.font = .font50PHeavy
@@ -66,6 +61,26 @@ final class WeeklyPercentView: UIView {
         return $0
     }(UIStackView())
     
+    private let bubbleView: UIView = {
+        $0.backgroundColor = .minningLightGray100
+        $0.layer.cornerRadius = 10
+        $0.isHidden = true
+        return $0
+    }(UIView())
+    
+    private let bubbleTriagleView: UIImageView = {
+        $0.image = UIImage(sharedNamed: "bubble_triangle_weekly")
+        $0.isHidden = true
+        return $0
+    }(UIImageView())
+    
+    private let bubbleLabel: UILabel = {
+        $0.text = "{(완료한 루틴×1)+(부분완료한 루틴×0.5)}/전체 루틴"
+        $0.textColor = .minningDarkGray100
+        $0.font = .font14P
+        return $0
+    }(UILabel())
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupView()
@@ -75,18 +90,31 @@ final class WeeklyPercentView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
+    @objc
+    private func onClickPercentGuideButton(_ sender: Any) {
+        bubbleTriagleView.isHidden.toggle()
+        bubbleView.isHidden.toggle()
+    }
+    
+    @objc
+    private func onClickBackground(_ sender: Any) {
+        guard bubbleTriagleView.isHidden == false, bubbleView.isHidden == false else { return }
+        bubbleTriagleView.isHidden = true
+        bubbleView.isHidden = true
+    }
+    
     private func setupView() {
+        isUserInteractionEnabled = true
+        addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(onClickBackground(_:))))
         backgroundColor = .primaryWhite
         
-        [descriptionLabel, percentStackView, titleStackView, resultStackView].forEach {
+        [descriptionLabel, percentStackView, titleLabel, percentGuideButton, resultStackView, bubbleTriagleView, bubbleView].forEach {
             addSubview($0)
         }
         [percentValueLabel, percentLabel].forEach {
             percentStackView.addArrangedSubview($0)
         }
-        [titleLabel, percentGuideButton].forEach {
-            titleStackView.addArrangedSubview($0)
-        }
+        bubbleView.addSubview(bubbleLabel)
         
         let completeView = WeeklyResultView(resultType: .complete, count: 10)
         let halfView = WeeklyResultView(resultType: .half, count: 10)
@@ -104,15 +132,35 @@ final class WeeklyPercentView: UIView {
             make.top.equalTo(descriptionLabel.snp.bottom)
             make.leading.equalTo(20)
         }
-        titleStackView.snp.makeConstraints { make in
+        titleLabel.snp.makeConstraints { make in
             make.top.equalTo(percentStackView.snp.bottom).offset(4)
             make.leading.equalTo(20)
         }
+        percentGuideButton.snp.makeConstraints { make in
+            make.centerY.equalTo(titleLabel)
+            make.leading.equalTo(titleLabel.snp.trailing).offset(-9)
+            make.width.height.equalTo(44)
+        }
         resultStackView.snp.makeConstraints { make in
             make.leading.trailing.equalToSuperview()
-            make.top.equalTo(titleStackView.snp.bottom).offset(52)
+            make.top.equalTo(titleLabel.snp.bottom).offset(52)
             make.bottom.equalTo(-20)
             make.height.equalTo(120)
+        }
+        bubbleTriagleView.snp.makeConstraints { make in
+            make.top.equalTo(percentGuideButton.snp.bottom).offset(-13)
+            make.centerX.equalTo(percentGuideButton)
+            make.width.equalTo(16.87)
+            make.height.equalTo(16.14)
+        }
+        bubbleView.snp.makeConstraints { make in
+            make.top.equalTo(bubbleTriagleView.snp.bottom).offset(-1)
+            make.leading.equalTo(12)
+            make.trailing.equalTo(-12)
+            make.height.equalTo(50)
+        }
+        bubbleLabel.snp.makeConstraints { make in
+            make.center.equalToSuperview()
         }
     }
 }
