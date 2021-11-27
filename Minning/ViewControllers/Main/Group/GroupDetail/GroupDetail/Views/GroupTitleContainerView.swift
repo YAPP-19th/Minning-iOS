82f7f2daa09aa9263d07c168d207de5ecf7b556d
koserim <btobang1@naver.com>
2021-11-28 00:21:20 +0900

    [YAPPIOS-154] 그룹 상세 관련 파일 위치 수정

---
+++ Minning/ViewControllers/Main/Group/GroupDetail/GroupDetail/Views/GroupTitleContainerView.swift
@@ -0,0 +1,21 @@
+//
+//  GroupTitleContainerView.swift
+//  Minning
+//
+//  Created by 고세림 on 2021/11/28.
+//  Copyright © 2021 Minning. All rights reserved.
+//
+
+import UIKit
+
+class GroupTitleContainerView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
