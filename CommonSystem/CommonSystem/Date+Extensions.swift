2ac55ea36a7db702a348861c6ae3fb2c701d331e
denny.k <denny.k@kakaocorp.com>
2021-12-19 00:23:36 +0900

    [YAPPIOS-217] 날짜 계산 로직 구현

--- CommonSystem/CommonSystem/Date+Extensions.swift
+++ CommonSystem/CommonSystem/Date+Extensions.swift
@@ -9,6 +9,58 @@
 import Foundation
 
 public extension Date {
+    static func createDate(year: Int, month: Int, day: Int,
+                           hour: Int = 0, minute: Int = 0, seconds: Int = 0) -> Date? {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        return formatter.date(from: "\(year)-\(month)-\(day) \(hour):\(minute):\(seconds)")
+    }
+    
+    func weeklySEDateList() -> [(Date, Date)]? {
+        var result: [(Date, Date)] = []
+        var startOfWeekDay: Int = 0
+        var calendar = Calendar(identifier: .gregorian)
+        calendar.locale = Locale(identifier: "ko")
+
+        let components = calendar.dateComponents([.year, .month], from: self)
+
+        if let startOfMonth = calendar.date(from: components),
+           let nextMonth = calendar.date(byAdding: .month, value: +1, to: startOfMonth),
+           let endOfMonth = calendar.date(byAdding: .day, value: -1, to: nextMonth) {
+            let comp1 = calendar.dateComponents([.day, .weekday], from: startOfMonth)
+            let comp2 = calendar.dateComponents([.day, .weekday], from: endOfMonth)
+            
+            let yearValue = calendar.dateComponents([.year, .month], from: startOfMonth).year
+            let monthValue = calendar.dateComponents([.year, .month], from: startOfMonth).month
+            
+            startOfWeekDay = comp1.weekday ?? 0
+            var tempList: [Date] = []
+            
+            for day in comp1.day!...comp2.day! {
+                let dayDate = Date.createDate(year: yearValue ?? 0, month: monthValue ?? 0, day: day) ?? Date()
+                
+                let dayWeekDay = dayDate.get(.weekday)
+                if dayWeekDay == startOfWeekDay || day == comp2.day ?? 0 {
+                    if day == comp2.day ?? 0 {
+                        tempList.append(dayDate)
+                    }
+                    
+                    if !tempList.isEmpty, let first = tempList.first, let last = tempList.last {
+                        result.append((first, last))
+                        tempList.removeAll()
+                        tempList.append(dayDate)
+                    } else {
+                        tempList.append(dayDate)
+                    }
+                } else {
+                    tempList.append(dayDate)
+                }
+            }
+        }
+        
+        return result
+    }
+    
     func convertToCustomString(format: String) -> String {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = format
