f6f5c6cc1a8ef6f21fcc35ee1fa02483e9bb5bcf
glossyyoon <mypju1128@gmail.com>
2021-12-29 16:58:33 +0900

    [YAPPIOS-241] 그룹 종료 코어데이터 생성, 수정 함수 추가

--- Minning/CoreData/GroupCoreDataManager.swift
+++ Minning/CoreData/GroupCoreDataManager.swift
@@ -11,15 +11,61 @@ import CoreData
 import Foundation
 
 class GroupCoreDataManager {
-    static var group: [EndedGroup] = []
+    static var endedGroupArray: [EndedGroup] = []
     
     static let shared: GroupCoreDataManager = GroupCoreDataManager()
-//    let appDelegate: AppDelegate? = UIApplication.shared.delegate as? AppDelegate
-//    lazy var context = appDelegate?.persistentContainer
-//    lazy var context = AppDelegate().persistentContainer
-//
-//    static func createEndedGroup(count: Int32) {
-//        let endedGroup = EndedGroup(context: <#T##NSManagedObjectContext#>)
-//        endedGroup.endedGroupCount = count
-//    }
+    lazy var context = AppDelegate().persistentContainer.viewContext
+    
+    static func initEndedGroup() {
+        let data = NSEntityDescription.entity(forEntityName: "EndedGroup", in: GroupCoreDataManager.shared.context)!
+        
+        let endedGroup = NSManagedObject(entity: data, insertInto: GroupCoreDataManager.shared.context)
+        endedGroup.setValue(true, forKeyPath: "isChecked")
+        endedGroup.setValue(0, forKeyPath: "endedGroupCount")
+    }
+
+    static func updateEndedGroup(count: Int16) {
+        let fetchRequest: NSFetchRequest<NSFetchRequestResult> = NSFetchRequest.init(entityName: "EndedGroup")
+        
+        do {
+            let data = try GroupCoreDataManager.shared.context.fetch(fetchRequest)
+            let entityModel = data[0] as! NSManagedObject
+            
+            if entityModel.value(forKey: "endedGroupCount") as! Int16 != count {
+                entityModel.setValue(count, forKey: "endedGroupCount")
+                entityModel.setValue(false, forKey: "isChecked")
+                
+                do {
+                    try GroupCoreDataManager.shared.context.save()
+                } catch {
+                    ErrorLog("\(error)")
+                }
+            }
+        } catch {
+            ErrorLog("\(error)")
+        }
+        
+    }
+    
+    static func updateIsChecked() {
+        let fetchRequest: NSFetchRequest<NSFetchRequestResult> = NSFetchRequest.init(entityName: "EndedGroup")
+        
+        do {
+            let data = try GroupCoreDataManager.shared.context.fetch(fetchRequest)
+            let entityModel = data[0] as! NSManagedObject
+            
+            if entityModel.value(forKey: "isChecked") as! Bool == false {
+                entityModel.setValue(true, forKey: "isChecked")
+                
+                do {
+                    try GroupCoreDataManager.shared.context.save()
+                } catch {
+                    ErrorLog("\(error)")
+                }
+            }
+        } catch {
+            ErrorLog("\(error)")
+        }
+    }
+
 }
